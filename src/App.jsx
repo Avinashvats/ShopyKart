05389711db866c320a6073be5d527e76c8ef04d5
Avinashvats <avinashvats000@gmail.com>
2022-10-17 14:30:06 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomePage from "./components/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <div>
       <HomePage />
+      <Footer />
     </div>
   );
 }
